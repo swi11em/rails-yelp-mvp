209b02dbdba6966edbdc0b49261a04d4f8769236
samuel willem <samuelwillem@hotmail.fr>
2024-11-13 13:04:24 +0100

    finish second exercice

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+Restaurant.create!(name: "La famille", address: "7 Boundary St, London E2 7JE", phone_number: "+0320304050",category: "french")
+Restaurant.create!(name: "Bombora", address: "165 avenue de bretagne, Lille", phone_number: "+0320304050",category: "japanese")
+Restaurant.create!(name: "FoodGood", address: "10 rue de l'égalité", phone_number: "+0320304050",category: "belgian")
+Restaurant.create!(name: "PicNic", address: "121 rue turgot", phone_number: "+0320304050",category: "italian")
+Restaurant.create!(name: "Woodl", address: "1à rue jean d'ormesson", phone_number: "+0320304050",category: "chinese")
+puts "Finished! Created #{Restaurant.count} restaurants."
